bac5728ed8d9fcf3942e953656ad2f55e1eb3ed1
robPTY <rcadown8@gmail.com>
2026-01-12 16:02:36 -0500

    updated tokenizer to allocate for bigger vocab

--- src/LSTM/seq2seq/demo.py
+++ src/LSTM/seq2seq/demo.py
@@ -6,32 +6,33 @@ sys.path.append('..')
 from tokenizer import Tokenizer
 from encoder import Encoder
 from decoder import Decoder
+from typing import Tuple
 
-VOCAB_SIZE = 300
-EMBEDDING_DIMS = 64
-HIDDEN_DIMS = 128
+VOCAB_SIZE = 8000
+EMBEDDING_DIMS = 256
+HIDDEN_DIMS = 512
+ATTENTION_DIMS = 256
 
-def load_model():
+def load_model() -> Tuple[Tokenizer, Encoder, Decoder]:
     print("Loading model...")
     
-    # Train tokenizer on corpus
-    df = pd.read_csv("data/eng_to_spa.txt", sep="\t", header=None, names=["english", "spanish", "meta"])
+    # Load tokenizer
     tokenizer = Tokenizer(vocab_size=VOCAB_SIZE)
-    tokenizer.tokenize(df["english"][:5000])
-    
+    tokenizer.load('weights/tokenizer_v1.pt')    
+
     # Load encoder & decoder
     encoder = Encoder(vocab_size=VOCAB_SIZE + 2, embedding_dim=EMBEDDING_DIMS, hidden_dim=HIDDEN_DIMS)
-    decoder = Decoder(vocab_size=VOCAB_SIZE + 2, embedding_dim=EMBEDDING_DIMS, hidden_dim=HIDDEN_DIMS)
-    encoder.load_weights('weights/encoder_final.pt')
-    decoder.load_weights('weights/decoder_final.pt')
+    decoder = Decoder(vocab_size=VOCAB_SIZE + 2, embedding_dim=EMBEDDING_DIMS, hidden_dim=HIDDEN_DIMS, attention_dim=ATTENTION_DIMS)
+    encoder.load_weights('weights/encoder_v3.pt')
+    decoder.load_weights('weights/decoder_v3.pt')
     
     print("Ready!\n")
     return tokenizer, encoder, decoder
 
-def translate(text, tokenizer, encoder, decoder):
+def translate(text: str, tokenizer: Tokenizer, encoder: Encoder, decoder: Decoder) -> str:
     tokens = tokenizer.encode(text)
-    hidden, cell = encoder.encode(tokens)
-    output = decoder.generate(hidden, cell, tokenizer.SOS, tokenizer.EOS, max_length=50)
+    encoder_outputs, hidden, cell = encoder.encode(tokens)
+    output = decoder.generate(hidden, cell, encoder_outputs, tokenizer.SOS, tokenizer.EOS, max_length=50)
     return tokenizer.decode(output)
 
 def main():
